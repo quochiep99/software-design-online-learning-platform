98443ebd4c00d96dd88cc3537c5f46d0073f8423
Quoc Hiep Doan <doanquochiep32@gmail.com>
2020-12-05 21:31:23 +0700

    create original file landing-o.hbs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.get("/", (req, res) => {
-    res.render("landing", {
+    res.render("landing-o", {
         layout: false
     });
 })
