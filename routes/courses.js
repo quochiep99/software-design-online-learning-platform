e67391e10196b972cc84028749185bfcf34b4492
Author Name <zzf7ktx@gmail.com>
2020-12-14 11:36:16 +0700

    refactor courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -2,56 +2,62 @@ const express = require("express");
 const Course = require("../models/course");
 const Field = require("../models/field");
 
-const router = express.Router({ mergeParams: true });
+const router = express.Router({
+    mergeParams: true
+});
 
 router.get("/", async (req, res) => {
-  const fieldName = req.params.field;
-  const field = await Field.findOne({ name: fieldName });
-  if (field) {
-    const requestedPage = req.query.page || 1;
-    const requestedLimit = parseInt(req.query.limit || 3);
-    const options = {
-      page: requestedPage,
-      limit: requestedLimit,
-      populate: ["field", "instructor", "reviews"],
-    };
-    Course.paginate({ field: field._id }, options, (err, result) => {
-      const courses = result.docs;
-      res.render("courses/index", {
-        result: result
-      });
+    const fieldName = req.params.field;
+    const field = await Field.findOne({
+        name: fieldName
     });
-  } else {
-    res.redirect("/");
-  }
+    if (field) {
+        const requestedPage = req.query.page || 1;
+        const requestedLimit = parseInt(req.query.limit || 3);
+        const options = {
+            page: requestedPage,
+            limit: requestedLimit,
+            populate: ["field", "instructor", "reviews"],
+        };
+        const result = await Course.paginate({ field: field._id }, options);
+        res.render("courses/index", {
+            result: result
+        });
+    } else {
+        res.redirect("/");
+    }
 });
 
 router.get("/:id", async (req, res) => {
-  const fieldName = req.params.field;
-  const field = await Field.findOne({ name: fieldName });
-  if (field) {
-    const course = await Course.findById(req.params.id)
-      .populate({
-        path: "reviews",
-        populate: {
-          path: "author",
-        },
-      })
-      .populate({ path: "students" })
-      .populate({
-        path: "instructor",
-      });
+    const fieldName = req.params.field;
+    const field = await Field.findOne({
+        name: fieldName
+    });
+    if (field) {
+        const course = await Course.findById(req.params.id)
+            .populate({
+                path: "reviews",
+                populate: {
+                    path: "author",
+                },
+            })
+            .populate({
+                path: "students"
+            })
+            .populate({
+                path: "instructor",
+            });
 
-    if (course) {
-      // increment views as users visit the course
-      course.numViews++;
-      await course.save();
-      return res.render("courses/show", {
-        course: course
-      });
+        if (course) {
+            // increment views as users visit the course
+            course.numViews++;
+            await course.save();
+            return res.render("courses/show", {
+                course: course
+            });
+        }
     }
-  }
-  res.redirect("/");
+    res.redirect("/");
 });
 
 module.exports = router;
