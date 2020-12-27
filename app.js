0894fe27bf5eb01d8ebaa62be6392723ffd47785
Author Name <zzf7ktx@gmail.com>
2020-12-27 16:45:17 +0700

    refactor exphbs helpers

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require("passport");
 const session = require('express-session');
-const moment = require("moment");
-const urlManipulator = require("./utils/url");
+// const moment = require("moment");
+// const urlManipulator = require("./utils/url");
+const helpers = require("./helpers/helpers");
 const flash = require('connect-flash');
 
 
@@ -43,69 +44,15 @@ app.engine('.hbs', exphbs({
         allowProtoMethodsByDefault: true
     },
     helpers: {
-        getTimeAgo: (date) => {
-            return moment(date).fromNow();
-        },
-        getLastUpdatedString: (date) => {
-            const newDate = new Date(date);
-            // return newDate.getDate() + "/" + (newDate.getMonth() + 1) + "/" + newDate.getFullYear();
-            const [hour, minute, second] = newDate.toLocaleTimeString("en-US").split(/:| /);
-            // return hour + "h : " + minute + "m : " + second + "s    " + newDate.toDateString();
-            return newDate.toTimeString();
-        },
-        calculateDiscountPercentage: (discountPrice, originalPrice) => {
-            return ((100 * (originalPrice - discountPrice)) / originalPrice).toFixed(0);
-        },
+        getTimeAgo: helpers.getTimeAgo,
+        getLastUpdatedString: helpers.getLastUpdatedString,
+        calculateDiscountPercentage: helpers.calculateDiscountPercentage,
 
         // function to convert i.e. 'web-development' into 'Web Development'
-        getFieldName: (fieldName) => {
-            return fieldName.replace("-", " ").replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
-        },
-        generatePagination: (result, currentURL) => {
-            var ret = "";
-            var previous = `<li class="left-etc"><a>&laquo;</a></li>`
-            if (result.hasPrevPage) {
-                previous = `<li class="left-etc"><a href=${currentURL.replace(`page=${result.page}`, `page=${result.prevPage}`)}>&laquo;</a></li>`
-            }
-            ret += previous;
-            for (var i = 1; i <= result.totalPages; i++) {
-                var pageHref = ""
-                if (!currentURL.includes("page")) {
-                    // add query string
-                    pageHref = urlManipulator.addParameterToURL(currentURL, "page", i);
-                } else {
-                    pageHref = urlManipulator.removeParameterFromURL(currentURL, "page");
-                    pageHref = urlManipulator.addParameterToURL(pageHref, "page", i);
-                }
-                // make the current page active and remove its href to make it unclickable since we are on this page
-                if (i === result.page) {
-                    ret += `<li class="active"><a>${i}</a></li>`
-                } else {
-                    ret += `<li><a href=${pageHref}>${i}</a></li>`
-                }
-            }
-            var next = `<li><a>&raquo;</a></li>`;
-            if (result.hasNextPage) {
-                next = `<li><a href=${currentURL.replace(`page=${result.page}`, `page=${result.nextPage}`)}>&raquo;</a></li>`;
-            }
-            ret += next;
-            return ret;
-        },
-        generateBestSeller: (totalStudents, options) => {
-            if (totalStudents >= 13) {
-                return options.fn();
-            }
-        },
-        generateNew: (updatedAt, options) => {
-            const today = new Date();
-            const date = new Date(updatedAt);
-            const diffTime = Math.abs(today - date);
-            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-            // if the last updated day is within last 3 days
-            if (diffDays <= 3) {
-                return options.fn();
-            }
-        }
+        getFieldName: helpers.getFieldName,
+        generatePagination: helpers.generatePagination,
+        generateBestSeller: helpers.generateBestSeller,
+        generateNew: helpers.generateNew
     }
 }));
 app.set('view engine', '.hbs');
@@ -151,7 +98,7 @@ app.use("/", indexRoutes);
 app.use("/it/:field/courses", courseRoutes)
 
 // Handle 404 - Keep this as a last route
-app.use(function (req, res, next) {    
+app.use(function (req, res, next) {
     res.render("404");
 });
 
