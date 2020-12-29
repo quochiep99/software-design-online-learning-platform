fc48788c118680091bd44d9614af4e23123c2ecc
Author Name <zzf7ktx@gmail.com>
2020-12-29 22:26:31 +0700

    add accordion #2

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -104,16 +104,26 @@ helpers.generateCurriculum = (curriculum) => {
     for (var i = 0; i < curriculum.children.length; i++) {
         const sectionName = curriculum.children[i].name;
         const sectionNumberInString = converter.toWords(i + 1);
+
+        var isHiddenClass = "collapse";
+        var isCollapsed = "plus"
+        // Show the first lesson
+        // const isCollapsed= (i===0) ? 
+
+        if (i === 0) {
+            isHiddenClass = "collapse show";
+            isCollapsed = "minus"
+        }
         str += `
         <div class="card">
         <div class="card-header" role="tab" id="heading${sectionNumberInString}">
             <h5 class="mb-0">
                 <a data-toggle="collapse" href="#collapse${sectionNumberInString}" aria-expanded="true"
-                    aria-controls="collapse${sectionNumberInString}"><i class="indicator ti-minus"></i>${sectionName}</a>
+                    aria-controls="collapse${sectionNumberInString}"><i class="indicator ti-${isCollapsed}"></i>${sectionName}</a>
             </h5>
         </div>
 
-        <div id="collapse${sectionNumberInString}" class="collapse show" role="tabpanel" aria-labelledby="heading${sectionNumberInString}"
+        <div id="collapse${sectionNumberInString}" class="${isHiddenClass}" role="tabpanel" aria-labelledby="heading${sectionNumberInString}"
             data-parent="#accordion_lessons">
             <div class="card-body">
                 <div class="list_lessons">
@@ -140,7 +150,7 @@ helpers.generateCurriculum = (curriculum) => {
 
 helpers.isInstructor = (user, course, options) => {
     if (user && user.role === "i") {
-        return options.fn({course});
+        return options.fn({ course });
     }
 }
 
