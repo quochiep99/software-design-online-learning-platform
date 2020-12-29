fb272110a69028bdb4e360696b1206dd36641b63
Author Name <zzf7ktx@gmail.com>
2020-12-29 17:18:03 +0700

    build curriculum #1

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,9 @@
 const urlManipulator = require("../utils/url");
 const moment = require("moment");
+const path = require("path");
+var converter = require('number-to-words');
+
+
 const helpers = [];
 helpers.getTimeAgo = (date) => {
     return moment(date).fromNow();
@@ -94,4 +98,44 @@ helpers.generateProfileOptions = (currentUser) => {
     }
 
 }
+
+helpers.generateCurriculum = (curriculum) => {
+    var str = "";
+    for (var i = 0; i < curriculum.children.length; i++) {
+        const sectionName = curriculum.children[i].name;
+        const sectionNumberInString = converter.toWords(i + 1);
+        str += `
+        <div class="card">
+        <div class="card-header" role="tab" id="heading${sectionNumberInString}">
+            <h5 class="mb-0">
+                <a data-toggle="collapse" href="#collapse${sectionNumberInString}" aria-expanded="true"
+                    aria-controls="collapse${sectionNumberInString}"><i class="indicator ti-minus"></i>${sectionName}</a>
+            </h5>
+        </div>
+
+        <div id="collapse${sectionNumberInString}" class="collapse show" role="tabpanel" aria-labelledby="heading${sectionNumberInString}"
+            data-parent="#accordion_lessons">
+            <div class="card-body">
+                <div class="list_lessons">
+                    <ul>
+                    `
+
+        for (var j = 0; j < curriculum.children[i].children.length; j++) {
+            const lessonName = path.parse(curriculum.children[i].children[j].name).name;
+            str += `
+            <li><a href="https://www.youtube.com/watch?v=LDgd_gUcqCw"
+                            class="video">${lessonName}</a><span>00:59</span></li>
+            `
+        }
+        str += `
+        </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+    }
+    return str;
+}
+
 module.exports = helpers;
