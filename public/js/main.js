e23a9360eaa5f73fe33d6339422710c35a41bbac
Author Name <zzf7ktx@gmail.com>
2020-12-30 15:25:28 +0700

    fix preloader to be faster

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,8 @@
 	
 	$(window).on('load', function () {
 		$('[data-loader="circle-side"]').fadeOut(); // will first fade out the loading animation
-		$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-		$('body').delay(350);
+		$('#preloader').fadeOut('fast'); // will fade out the white DIV that covers the website.
+		$('body');
 		$('#hero_in h1,#hero_in form').addClass('animated');
 		$('.hero_single, #hero_in').addClass('start_bg_zoom');
 		$(window).scroll();
