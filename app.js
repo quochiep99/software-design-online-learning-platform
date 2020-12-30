193aec2fc412b3d75a014ddc8eecb4d1e94c8636
Quoc Hiep Doan <doanquochiep32@gmail.com>
2020-12-30 09:56:43 +0700

    refactor email confirmation #1

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(function (req, res, next) {
     if (req.user) {
         res.locals.currentUser = req.user;
     }
-    if (!req.url.includes("/login") && !req.url.includes("/logout") && !req.url.includes("/register") && !req.url.includes("/favicon.ico") && !req.url.includes("img/logo@2x.png")) {
+    if (req.url.includes("/it") || req.url.includes("/search") || req.url.includes("/my-courses") || req.url.includes("/profile") || req.url.includes("/courses")) {
         req.session.returnTo = req.url;
     }
     res.locals.currentURL = req.url;
