967ddd39246e29ec648888bf4e5301561e06b998
Quoc Hiep Doan <doanquochiep32@gmail.com>
2020-12-31 16:14:21 +0700

    add rating star #5

--- helpers/getLastUpdatedString.js
+++ helpers/getLastUpdatedString.js
@@ -2,6 +2,6 @@ module.exports = (date) => {
     const newDate = new Date(date);
     // return newDate.getDate() + "/" + (newDate.getMonth() + 1) + "/" + newDate.getFullYear();
     const [hour, minute, second] = newDate.toLocaleTimeString("en-US").split(/:| /);
-    // return hour + "h : " + minute + "m : " + second + "s    " + newDate.toDateString();
-    return newDate.toTimeString();
+    return hour + "h : " + minute + "m : " + second + "s    " + newDate.getDate() + "/" + (newDate.getMonth() + 1) + "/" + newDate.getFullYear();
+    // return newDate.toTimeString();
 }
