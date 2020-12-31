c5dfa89509554ae88ece6785c62b7db302e90473
Author Name <zzf7ktx@gmail.com>
2020-12-31 14:58:17 +0700

    add rating star #3

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,3 +1,4 @@
+// Most featured courses
 document.querySelectorAll(".owl-stage > div").forEach(e => {
     const courseRating = parseFloat(e.querySelector(".rating span").textContent);
     const element = e.querySelector(".ratingStar");
@@ -6,7 +7,7 @@ document.querySelectorAll(".owl-stage > div").forEach(e => {
         step: 0.1,
         rating: courseRating,
         element: element,
-        readOnly:true, 
+        readOnly: true,
         rateCallback: function rateCallback(rating, done) {
             this.setRating(rating);
             done();
@@ -14,4 +15,21 @@ document.querySelectorAll(".owl-stage > div").forEach(e => {
     });
 })
 
-
+// Most viewed and recent courses
+for (var i = 4; i <= 5; i++) {
+    document.querySelectorAll(`div:nth-of-type(${i}) > .row > div`).forEach(e => {
+        const courseRating = parseFloat(e.querySelector(".rating span").textContent);
+        const element = e.querySelector(".ratingStar");
+        var starRatingStep = raterJs({
+            starSize: 16,
+            step: 0.1,
+            rating: courseRating,
+            element: element,
+            readOnly: true,
+            rateCallback: function rateCallback(rating, done) {
+                this.setRating(rating);
+                done();
+            }
+        });
+    })
+}
