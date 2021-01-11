27d69681be1bee5a9249b0b3b8da4a45085d4afc
Author Name <zzf7ktx@gmail.com>
2021-01-11 16:30:32 +0700

    add returnTo to Google

--- routes/index.js
+++ routes/index.js
@@ -201,7 +201,8 @@ router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 
 router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
     function (req, res) {
         // Successful authentication, redirect home.
-        res.redirect('/');
+        res.redirect(req.session.returnTo || "/");
+        delete req.session.returnTo;
     });
 
 
