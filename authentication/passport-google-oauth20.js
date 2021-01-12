1f6a660ae83263cecfe275b1c6df8fa18476f31a
Quoc Hiep Doan <doanquochiep32@gmail.com>
2021-01-12 09:52:05 +0700

    add google callback

--- authentication/passport-google-oauth20.js
+++ authentication/passport-google-oauth20.js
@@ -17,7 +17,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.CALLBACK_URL || "http://localhost:3000/auth/google/callback"
 },
     async function (accessToken, refreshToken, profile, done) {
         try {
