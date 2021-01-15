0af5a975be31eb8d86164a65646cce90e0ec03cd
Author Name <zzf7ktx@gmail.com>
2021-01-15 20:26:08 +0700

    add course curriculum data to show and preview page

--- helpers/generateCourseUnits.js
+++ helpers/generateCourseUnits.js
@@ -1,12 +1,53 @@
 const converter = require('number-to-words');
 const path = require("path");
 module.exports = (course, curriculum, currentLessonName, isPreviewMode) => {
-    if ((!curriculum) || (Object.keys(curriculum).length === 0 && curriculum.constructor === Object)) {
-        return "No curriculum has been uploaded !!!"
+    var str = "";
+    // If no curriculum is uploaded, then we use the default course curriculum
+    if ((!curriculum) || ("defaultCurriculum" in curriculum) || (Object.keys(curriculum).length === 0 && curriculum.constructor === Object)) {
+        curriculum = curriculum.defaultCurriculum;
+        for (var i = 0; i < curriculum.length; i++) {
+            const sectionName = curriculum[i].sectionName;
+            const sectionNumberInString = converter.toWords(i + 1);
+            str += `
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-heading${sectionNumberInString}">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapse${sectionNumberInString}" aria-expanded="false"
+                        aria-controls="flush-collapse${sectionNumberInString}">
+                        <b>${sectionName}</b>
+                    </button>
+                </h2>
+                <div id="flush-collapse${sectionNumberInString}" class="accordion-collapse collapse"
+                    aria-labelledby="flush-heading${sectionNumberInString}" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                        <div class="list-group">
+            `
+            for (var j = 0; j < curriculum[i].unitNames.length; j++) {
+                const lessonName = curriculum[i].unitNames[j];
+                str += `                        
+                    <a href="#" class="list-group-item list-group-item-action">                
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">${lessonName}</a>
+                    `
+            }
+            str += `
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            if (isPreviewMode) {
+                str = str.
+                    replace(/learn/g, "preview").
+                    replace(/class="form-check-input" id="exampleCheck1"/g, `class="form-check-input" id="exampleCheck1" style="display:none"`);
+                break;
+            }
+        };
+        return str;
     }
 
-    var str = "";
 
+
+    // Instructors have uploaded their curriculum
     for (var i = 0; i < curriculum.children.length; i++) {
         const sectionName = curriculum.children[i].name;
         const sectionNumberInString = converter.toWords(i + 1);
--- helpers/generateCurriculum.js
+++ helpers/generateCurriculum.js
@@ -1,11 +1,56 @@
 const converter = require('number-to-words');
 const path = require("path");
 module.exports = (curriculum) => {
-    if ((!curriculum) || (Object.keys(curriculum).length === 0 && curriculum.constructor === Object)) {
-        return "No curriculum has been uploaded !!!"
+    var str = "";
+    // If no curriculum is uploaded, then we use the default course curriculum
+    if ((!curriculum) || ("defaultCurriculum" in curriculum) || (Object.keys(curriculum).length === 0 && curriculum.constructor === Object)) {
+        curriculum = curriculum.defaultCurriculum;
+        for (var i = 0; i < curriculum.length; i++) {
+            const sectionName = curriculum[i].sectionName;
+            const sectionNumberInString = converter.toWords(i + 1);
+
+            var isHiddenClass = "collapse";
+            var isCollapsed = "plus"
+            // Show the first lesson        
+
+            if (i === 0) {
+                isHiddenClass = "collapse show";
+                isCollapsed = "minus"
+            }
+            str += `
+            <div class="card">
+            <div class="card-header" role="tab" id="heading${sectionNumberInString}">
+                <h5 class="mb-0">
+                    <a data-toggle="collapse" href="#collapse${sectionNumberInString}" aria-expanded="true"
+                        aria-controls="collapse${sectionNumberInString}"><i class="indicator ti-${isCollapsed}"></i>${sectionName}</a>
+                </h5>
+            </div>
+    
+            <div id="collapse${sectionNumberInString}" class="${isHiddenClass}" role="tabpanel" aria-labelledby="heading${sectionNumberInString}"
+                data-parent="#accordion_lessons">
+                <div class="card-body">
+                    <div class="list_lessons">
+                        <ul>
+                        `
+
+            for (var j = 0; j < curriculum[i].unitNames.length; j++) {
+                const lessonName = curriculum[i].unitNames[j];
+                str += `
+                <li><a class="video">${lessonName}</a></li>
+                `
+            }
+            str += `
+            </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+            `
+        }
+        return str;
     }
 
-    var str = "";
+    // Instructors have uploaded their curriculum
     for (var i = 0; i < curriculum.children.length; i++) {
         const sectionName = curriculum.children[i].name;
         const sectionNumberInString = converter.toWords(i + 1);
